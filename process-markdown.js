85d81207811227708ec4bf47327244d1fc8f9e10
rjrobben <rjrobben123@gmail.com>
2025-06-15 21:46:45 +0800

    update

--- process-markdown.js
+++ process-markdown.js
@@ -102,7 +102,7 @@ async function processMarkdown(markdownFilePath) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${postTitle} - My Simple Blog</title>
+    <title>${postTitle}</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
